b4dfa3280de8b7e45e0a5278c94ee44b538a0931
saicharan8855 <saicharan9948644390@gmail.com>
2026-03-27 01:02:11 +0530

    edit preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,19 +1,23 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler , LabelEncoder
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 
-l_e = LabelEncoder()
-df['smoker'] = l_e.fit_transform(df['smoker'])
-df['sex'] = l_e.fit_transform(df['sex'])
-df['region'] = l_e.fit_transform(df['region'])
-
-
-X = df.drop(columns = 'charges')
-y = df['charges']
-
-X_train , X_test , y_train , y_test = train_test_split(X , y , test_size = 0.2 , random_state = 99)
-
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+def preprocess():
+    df = pd.read_csv('data/insurance.csv')
+    
+    le = LabelEncoder()
+    df['smoker'] = le.fit_transform(df['smoker'])
+    df['sex'] = le.fit_transform(df['sex'])
+    df['region'] = le.fit_transform(df['region'])
+    
+    X = df.drop(columns='charges')
+    y = df['charges']
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=99)
+    
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+    
+    return X_train, X_test, y_train, y_test, scaler
